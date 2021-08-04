67bb292b47b5db3a4104236abe66684c699cb8e5
lucasrpb <lucasrpb@gmail.com>
2021-08-04 16:03:31 -0300

    Playing around with the github API for knowing how it works...

--- src/main/scala/io/scalac/githubrank/Main.scala
+++ src/main/scala/io/scalac/githubrank/Main.scala
@@ -10,7 +10,7 @@ import akka.http.scaladsl.model.HttpProtocols._
 import akka.http.scaladsl.model.MediaTypes._
 import akka.http.scaladsl.model.HttpCharsets._
 import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.headers.{Authorization, GenericHttpCredentials, RawHeader}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
@@ -28,30 +28,55 @@ object Main {
     val logger = LoggerFactory.getLogger(this.getClass)
 
     val GitHubHeader = RawHeader("Accept", "application/vnd.github.v3+json")
+    val AuthHeader = Authorization(GenericHttpCredentials("token", "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"))
 
     implicit val system = ActorSystem.create[Nothing](Behaviors.empty[Nothing], "GitHubClient")
     implicit val ec = system.executionContext
 
+    case class Repository(name: String, full_name: String)
+    case class Contribution(login: String, contributions: Int)
+
+    implicit val repositoryFormat = jsonFormat2(Repository)
+    implicit val contributorFormat = jsonFormat2(Contribution)
+
     val http = Http()
 
-    def getReposByOrg(org: String): Future[String] = {
+    def getContributorsByRepo(repo: Repository, page: Int): Future[Seq[Contribution]] = {
       val request =
-        Get(s"https://api.github.com/orgs/$org/repos")
-        .withHeaders(Seq(
-          GitHubHeader
-        ))
-
-      http.singleRequest(request).flatMap(response => Unmarshal(response.entity).to[ByteString].map(_.utf8String.parseJson.prettyPrint))
-    }
-
-    def getContributorsByRepo(org: String, repo: String): Future[String] = {
-      val request =
-        Get(s"https://api.github.com/repos/$org/$repo/contributors")
+        Get(s"https://api.github.com/repos/${repo.full_name}/contributors?page=$page&per_page=50")
           .withHeaders(Seq(
             GitHubHeader
           ))
 
-      http.singleRequest(request).flatMap(response => Unmarshal(response.entity).to[ByteString].map(_.utf8String.parseJson.prettyPrint))
+      println(s"checking contributions for repo ${repo}")
+
+      http.singleRequest(request).flatMap(response => Unmarshal(response.entity).to[ByteString]
+        .map(_.utf8String.parseJson.convertTo[Seq[Contribution]]))
+    }
+
+    def getAllContributorsByRepo(repo: Repository, page: Int = 1): Future[Seq[Contribution]] = {
+      getContributorsByRepo(repo, page).flatMap {
+        case list if list.isEmpty => Future.successful(list)
+        case list => getAllContributorsByRepo(repo, page + 1).map(list ++ _)
+      }
+    }
+
+    def getReposByOrg(org: String, page: Int): Future[Seq[Repository]] = {
+      val request =
+        Get(s"https://api.github.com/orgs/$org/repos?page=$page&per_page=50")
+          .withHeaders(Seq(
+            GitHubHeader,
+            AuthHeader
+          ))
+
+      http.singleRequest(request).flatMap(response => Unmarshal(response.entity).to[ByteString].map(_.utf8String.parseJson.convertTo[Seq[Repository]]))
+    }
+
+    def getAllReposByOrg(org: String, page: Int = 1): Future[Seq[Repository]] = {
+      getReposByOrg(org, page).flatMap {
+        case list if list.isEmpty => Future.successful(list)
+        case list => getAllReposByOrg(org, page + 1).map(list ++ _)
+      }
     }
 
     /*(for {
@@ -69,17 +94,20 @@ object Main {
       case Failure(ex) => logger.error(ex.getMessage)
     }*/
 
-    Future.sequence(Seq(
-      //getReposByOrg("ScalaConsultants"),
-      getContributorsByRepo("ScalaConsultants","docker-postgres")
-    )).onComplete {
-      case Success(data) =>
+    getAllReposByOrg("ScalaConsultants").onComplete {
+      case Success(repos) =>
 
         //logger.info(s"response: ${json.prettyPrint}\n")
 
-        println(data)
+        Future.sequence(repos.map(r => getAllContributorsByRepo(r).map(r -> _))).onComplete {
+          case Success(list) =>
 
-        system.terminate()
+            println(list)
+
+            system.terminate()
+
+          case Failure(ex) => throw ex
+        }
 
       case Failure(ex) => logger.error(ex.getMessage)
     }
